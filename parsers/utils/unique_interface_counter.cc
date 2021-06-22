3b909818cb2524eda43fad1ecc4b5737cfde5c1a
Neo Huang <searchingundermoon@gmail.com>
2021-06-21 23:04:44 -0700

    fixed an issue

--- parsers/utils/unique_interface_counter.cc
+++ parsers/utils/unique_interface_counter.cc
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <cstring> // for std::strlen
 #include <cstddef> // for std::size_t -> is a typedef on an unsinged int
+#include <cstring> // for std::strlen
+#include <fstream>
+#include <iostream>
 #include <unordered_set>
+#include <vector>
 
 #include "glog/logging.h"
 #include "absl/flags/usage.h"
@@ -19,7 +20,8 @@ using flashroute::Ipv4Address;
 using flashroute::IpAddressHash;
 using flashroute::IpAddressEquality;
 
-ABSL_FLAG(std::string, target_file, "", "Dump targets to file.");
+ABSL_FLAG(std::vector<std::string>, target_file, std::vector<std::string>{},
+          "Dump targets to file.");
 
 struct DataElement {
   uint32_t destination[4];
@@ -36,51 +38,41 @@ int main(int argc, char* argv[]) {
   absl::SetProgramUsageMessage("This program does nothing.");
   absl::ParseCommandLine(argc, argv);
 
-  LOG(INFO) << "Start to read data from: " << absl::GetFlag(FLAGS_target_file);
-  flashroute::ResultDumper resultDumper{
-      absl::StrCat(absl::GetFlag(FLAGS_target_file), "_unique_interface")};
 
   std::unordered_set<IpAddress *, IpAddressHash, IpAddressEquality> observedInterface;
 
   std::ifstream inFile;
-  inFile.open(absl::GetFlag(FLAGS_target_file),
-              std::ios::in | std::ios::binary);
-  DataElement buffer;
+  auto tarGetFiles = absl::GetFlag(FLAGS_target_file);
   uint64_t records = 0;
   uint64_t interface = 0;
-  uint64_t dumped = 0;
-  while (inFile.peek() != EOF) {
-    inFile.read(reinterpret_cast<char *>(&buffer), 39);
-    records++;
-    if (buffer.ipv4 == 1) {
+  for (auto file : tarGetFiles) {
+    LOG(INFO) << "Start to read data from: " << file;
+    inFile.open(file, std::ios::in | std::ios::binary);
+    DataElement buffer;
+    while (inFile.peek() != EOF) {
+      inFile.read(reinterpret_cast<char *>(&buffer), 39);
+      records++;
+      if (buffer.ipv4 == 1) {
         // IPv4 address handling.
         auto addr = new Ipv4Address(buffer.responder[0]);
         if (buffer.fromDestination == true) {
-          dumped++;
-          resultDumper.scheduleDumpData(
-              *addr, Ipv4Address{buffer.destination[0]}, buffer.distance, buffer.rtt,
-              static_cast<bool>(buffer.fromDestination), true, nullptr, 0);
         } else if (observedInterface.find(addr) == observedInterface.end()) {
           observedInterface.insert(addr);
-          dumped++;
-          resultDumper.scheduleDumpData(
-              *addr, Ipv4Address{buffer.destination[0]}, buffer.distance, buffer.rtt,
-              static_cast<bool>(buffer.fromDestination), true, nullptr, 0);
           if (buffer.fromDestination == 0)
             interface += 1;
 
         } else {
           delete addr;
         }
-    } else {
+      } else {
         // IPv6 address handling
         // TODO: we need to add the code logic handle IPv6 Address.
+      }
     }
-  }
 
-  inFile.close();
+    inFile.close();
+  }
   LOG(INFO) << "Processed " << records << " records.";
-  LOG(INFO) << "Dumped " << dumped << " records.";
   LOG(INFO) << "There are " << interface << " unique interfaces.";
 
 }
